20b622b9d43ecb28c72d3d9437b3beaacca23563
전동원 <jdw7645@naver.com>
2024-04-14 20:53:07 +0900

    Create Screen6_1(Wrong Route)

---
+++ Android/app/src/main/java/com/taxitogether/app/screen6_1.java
@@ -0,0 +1,33 @@
+package com.taxitogether.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class screen6_1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button1(View v){
+        Intent intent = new Intent(getApplicationContext(), screen4.class);
+        startActivity((intent));
+        finish();
+    }
+
+    public void button2(View v){
+        Intent intent = new Intent(getApplicationContext(), screen4.class);
+        startActivity((intent));
+        finish();
+    }
+
+    public void button3(View v){
+        Intent intent = new Intent(getApplicationContext(), screen1.class);
+        startActivity((intent));
+        finish();
+    }
+}

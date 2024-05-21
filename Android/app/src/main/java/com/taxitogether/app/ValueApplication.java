a5f8c949e5007592a414303ebc3d5c93e69c9502
전동원 <jdw7645@naver.com>
2024-05-21 22:35:10 +0900

    block_dark_mode

--- Android/app/src/main/java/com/taxitogether/app/ValueApplication.java
+++ Android/app/src/main/java/com/taxitogether/app/ValueApplication.java
@@ -1,12 +1,14 @@
 package com.taxitogether.app;
 
 import android.app.Application;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class ValueApplication extends Application {
 
     private int num_of_person;
     @Override public void onCreate(){
         super.onCreate();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         num_of_person=2;
     }
     public int get_num(){
